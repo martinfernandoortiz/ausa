367bef1778342dfea2f50a8a7bb1b2073fb28deb
Martín Fernando Ortiz <38224115+martinfernandoortiz@users.noreply.github.com>
2024-08-27 12:17:45 -0300

    Create accidentes.sql

---
+++ sql/accidentes.sql
@@ -0,0 +1,35 @@
+
+CREATE OR REPLACE VIEW geodatos.v_incidentes AS
+WITH joined_data AS (
+    SELECT
+        i.*,
+        n.tramo_cod
+    FROM
+        geodatos.incidentes i
+    LEFT JOIN
+        geodatos.nomenclador n
+    ON
+        i.autopista = n.autopista
+)
+SELECT
+    
+	 ROW_NUMBER() OVER () AS gid,  -- Genera un ID único para cada fila
+	--vp.gid,
+	vp.geom,
+	vp.id_tramo,
+	vp.cod_tramo,
+	vp.nom_tramo,
+	vp.pk as pk_pk,
+	vp.pk_numerica,
+    jd.*
+FROM
+    gisdata.v_progresivas vp
+RIGHT JOIN
+    joined_data jd
+ON
+    vp.cod_tramo = jd.tramo_cod
+    AND vp.cod_banda = jd.eje_desc
+    AND vp.pk_numerica = CASE
+                            WHEN jd.pk ~ '^[0-9]+$' THEN jd.pk::integer
+                            ELSE NULL
+                         END;
