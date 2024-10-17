f7d125eea70850caa6cafc62bc425a1257ad40f4
Martín Fernando Ortiz <38224115+martinfernandoortiz@users.noreply.github.com>
2024-10-17 09:35:12 -0300

    Update rename_columns.sql

--- sql/rename_columns.sql
+++ sql/rename_columns.sql
@@ -1,16 +1,15 @@
-## Pasar columnas a minuscula y sacarle el espacio
 DO $$ 
 DECLARE
     column_old_name TEXT;
     column_new_name TEXT;
-    table_name TEXT := 'siniestros';
+    table_name_var TEXT := 'siniestros'; -- Renombramos la variable a table_name_var
     schema_name TEXT := 'gisdata';
 BEGIN
     FOR column_old_name IN
         SELECT column_name
         FROM information_schema.columns
         WHERE table_schema = schema_name
-          AND table_name = table_name
+          AND table_name = table_name_var -- Usamos el nombre de la variable modificada
     LOOP
         -- Convertir a minúsculas y reemplazar espacios por guiones bajos
         column_new_name := lower(replace(column_old_name, ' ', '_'));
@@ -20,7 +19,7 @@ BEGIN
             EXECUTE format(
                 'ALTER TABLE %I.%I RENAME COLUMN %I TO %I',
                 schema_name,
-                table_name,
+                table_name_var, -- Usamos la variable modificada
                 column_old_name,
                 column_new_name
             );
