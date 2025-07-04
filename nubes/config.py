b6c95542423a40497de67c5e4089d88dba054008
Martín Fernando Ortiz <38224115+martinfernandoortiz@users.noreply.github.com>
2025-07-04 15:15:10 -0300

    Update config.py

--- nubes/config.py
+++ nubes/config.py
@@ -1,5 +1,11 @@
-filename = "larrazabal_cividis.las"
-f = open(filename, "rb+")
-f.seek(6)
-f.write(bytes([17, 0, 0, 0]));
-f.close()
+import sys
+
+if len(sys.argv) < 2:
+    print("Uso: python3 config.py <nombre_archivo.las>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+with open(filename, "rb+") as f:
+    f.seek(6)
+    f.write(bytes([17, 0, 0, 0]))
